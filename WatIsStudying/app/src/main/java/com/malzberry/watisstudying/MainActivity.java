21c34b43d3bd344229cc47b9f78fb1c8dc0cf351
AmyDZeng <amy.d.zeng@gmail.com>
2016-01-23 02:42:54 -0500

    added small layout changes

--- WatIsStudying/app/src/main/java/com/malzberry/watisstudying/MainActivity.java
+++ WatIsStudying/app/src/main/java/com/malzberry/watisstudying/MainActivity.java
@@ -43,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new OneFragment(), "ONE");
-        adapter.addFragment(new TwoFragment(), "TWO");
-        adapter.addFragment(new ThreeFragment(), "THREE");
+        adapter.addFragment(new OneFragment(), "RECENT");
+        adapter.addFragment(new TwoFragment(), "HOTTEST");
+        adapter.addFragment(new ThreeFragment(), "STARRED");
         viewPager.setAdapter(adapter);
     }
 
--- WatIsStudying/app/src/main/java/com/malzberry/watisstudying/OneFragment.java
+++ WatIsStudying/app/src/main/java/com/malzberry/watisstudying/OneFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 // import info.androidhive.materialtabs.R;
 
 
 public class OneFragment extends Fragment{
-
+    String[] StringArray = {"1"};
     public OneFragment() {
         // Required empty public constructor
     }
@@ -18,6 +20,11 @@ public class OneFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.list_item, StringArray);
+
+        ListView listView = (ListView) this.getActivity().findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 
     @Override
